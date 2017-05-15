982aa8cedd98357f8e66af2b4a61a0be291f7a4f
Nikola Ravic <nikola.ravic@gmail.com>
2017-05-15 23:56:38 +0200

    minor fix

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ let express = require('express'),
   bodyParser = require('body-parser'),
   app = express(),
   initRoutes = require('./routes/user.server.routes'),
+  config = require('config'),
   staticFiles = express.static('public');
 
 // using middlewares: *morgan to log every request in console and express.static to serve static files, body parser: parse request payload
@@ -14,6 +15,6 @@ app.use(morgan('tiny'));
 app.use(staticFiles);
 initRoutes(app);
 
-app.listen(3333, ()=>{
+app.listen(config.PORT, ()=>{
   console.log('Server started on port 3333');
 });
