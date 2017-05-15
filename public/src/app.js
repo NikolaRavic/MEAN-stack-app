9f9e5cd7c092275e3ea7927a4a0510270609d7da
Nikola Ravic <nikola.ravic@gmail.com>
2017-05-15 23:42:51 +0200

    added some comments

--- public/src/app.js
+++ public/src/app.js
@@ -5,6 +5,7 @@
     app.run(()=> {
       console.log('Greetings from Angular! Have a nice day!');
     })
+      //set theme palette for styling
       .config(function($mdThemingProvider) {
       $mdThemingProvider.theme('default')
         .primaryPalette('blue')
